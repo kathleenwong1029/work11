33a85e50fd2b56b0f0bf0854737526a682e80ddd
jkim33 <jkim33@stuy.edu>
2018-10-31 00:19:03 -0400

    done

--- stat.c
+++ stat.c
@@ -7,20 +7,20 @@
 #include <math.h>
 
 int main(){
-  int file = open("text.txt",O_CREAT, 0666);
   struct stat stats;
-  stat("text.txt", &stats);
-  printf("file size:%lld\n", stats.st_size);
+  stat("stat.c", &stats);
+  printf("file size:%ld\n", stats.st_size);
   printf("mode(permissions):%o\n", stats.st_mode);
   printf("last time of access:%s\n", ctime(&stats.st_atime));
   //Modify the size so it is printed out in human readable form
   size_t size = stats.st_size;
   char *str = malloc(256);
-  int gb  = size / 1024;
-  int mb = size / (int) pow(1024,3);
-  int kb = size / (int) pow(1024,6);
-  int b = size / (int) pow(1024,9);
+  int gb  = size / pow(1024,3);
+  int mb = size / pow(1024,2);
+  int kb = size / 1024;
+  int b = size;
   sprintf(str, "file size: %dGB, %dMB, %dKB, %dB", gb, mb, kb, b);
   printf("%s\n", str);
+  printf("mode(permissions): %o\n", stats.st_mode & 511); //511 in binary is 111111111
 
 }
